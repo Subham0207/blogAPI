f5cb9b73036d526165e2036b58852e696cd1917f
SUBHAM <subhamckp12@gmail.com>
2021-10-07 20:42:12 +0530

    endpoint - get blog with most views
    get blog with most views, if blogs have same number of views
    then get the lastest blog(highest date value) among them.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from blog.models import blog
-from .views import blogView, blog_filterbytitle_view, blog_returnFirstTen_view, blog_incViews_view
+from .views import blogView, blog_filterbytitle_view, blog_returnFirstTen_view, blog_incViews_view, blog_getMostViewedblog
 
 urlpatterns = [
     # path('blog/getAll',include('blog/urls.py')),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('getall/filter',blog_filterbytitle_view.as_view()),
     path('getall/getfirstten',blog_returnFirstTen_view.as_view()),
     path('getall/incview',blog_incViews_view.as_view()),
+    path('getall/mostviewed',blog_getMostViewedblog.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,12 +7,12 @@ from .serializer import blogSerializer,filterSerializer, idSerializer
 
 # Create your views here.
 class blogView(APIView):
-    def get(self,req):
+    def get(self,req):#Get all the blogs
         blogs = blog.objects.all()
         serializer = blogSerializer(blogs,many=True)
         return Response(serializer.data)
 
-    def post(self,req):
+    def post(self,req):#Add a blog
         new_blog_serializer = blogSerializer(data = req.data)
         if new_blog_serializer.is_valid():
             new_blog_serializer.save()
@@ -20,7 +20,7 @@ class blogView(APIView):
         return Response(new_blog_serializer.errors)
 
 
-class blog_filterbytitle_view(APIView):
+class blog_filterbytitle_view(APIView):#title is passed as param
     def get(self,req):
         serializer = filterSerializer(data = req.query_params)#json to dict
         if serializer.is_valid():
@@ -31,13 +31,13 @@ class blog_filterbytitle_view(APIView):
             return Response(serializer2.data)#dict to jsons
 
 
-class blog_returnFirstTen_view(APIView):
+class blog_returnFirstTen_view(APIView):#returns 10 latest blog
     def get(self,req):
         serializer = blogSerializer(blog.objects.all().order_by('-date')[:10],many=True)
         return Response(serializer.data)
 
-#This implementation Update is not recommended
-class blog_incViews_view(APIView):
+#This implementation of Update is not recommended
+class blog_incViews_view(APIView):#on blog visit view is increased
     def put(self,req):
         #getting the entry using id
         serializer = idSerializer(data = req.data)
@@ -50,3 +50,9 @@ class blog_incViews_view(APIView):
             blog.objects.filter(id=serializer.data["id"]).update(views=serializer2.data[0]["views"])
             
             return Response(serializer2.data)
+
+class blog_getMostViewedblog(APIView):#returns most blog with most views
+    def get(self,req):
+        serializer = blogSerializer(blog.objects.all().order_by("-views","-date")[:1],many=True)# "neg" means desc 
+        return Response(serializer.data)
+
