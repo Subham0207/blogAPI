05f1a48d4b049a9cfe358da03563635acaae3b0b
SUBHAM <subhamckp12@gmail.com>
2021-10-07 12:19:07 +0530

    blog app - model, serializer

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import datetime
+from django.utils.timezone import now
 
 # Create your models here.
+
+class blog(models.Model):
+    title = models.CharField(max_length=100)
+    date = now
+    author = models.CharField(max_length=50)
+    img_author = models.ImageField()
+    img_blog = models.ImageField()
+    views = models.IntegerField(default=0)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.author
+
+
+
