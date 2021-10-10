00174e0c97ae81184fc86764b247717e9017b626
SUBHAM <subhamckp12@gmail.com>
2021-10-10 12:44:15 +0530

    changed blog model to save images in media folder

--- pracAPI/urls.py
+++ pracAPI/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from getpost import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('employee/',views.employeelist.as_view()),
     path('employee/getDetail',views.employeeDetails.as_view()),
     path('blog/',include('blog.urls')),
-]
+] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
